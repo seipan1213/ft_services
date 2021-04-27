09e123cb2bbe1726dd18690b053c46de2daf56d7
seipan1213 <seiju1213@gmail.com>
2021-04-27 21:20:30 +0900

    ftps done

--- srcs/nginx/srcs/setup.sh
+++ srcs/nginx/srcs/setup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 telegraf --config /etc/telegraf/telegraf.conf&
+rc-update add sshd
+rc-status
+/etc/init.d/sshd start
 nginx -g 'daemon off;'
--- srcs/wordpress/srcs/setup.sh
+++ srcs/wordpress/srcs/setup.sh
@@ -4,4 +4,24 @@ rc-status
 rc-service php-fpm7 start
 telegraf --config /etc/telegraf/telegraf.conf&
 nginx -g 'daemon off;'
+
+cd /var/www/wordpress
+
+wp core install --title=test --admin_user=admin --admin_password=admin --admin_email=admin@example.com --url=https://192.168.49.51:5050
+
+wp user create rika \
+	rika@example.com \
+	--role=editor \
+	--user_pass=rika
+
+wp user create rena \
+	rena@example.com \
+	--role=author \
+	--user_pass=rena
+
+wp user create keichi \
+	keichi@example.com \
+	--role=author \
+	--user_pass=keichi
+
 sleep infinity
